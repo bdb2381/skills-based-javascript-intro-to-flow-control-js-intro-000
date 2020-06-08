44e16b8fd4fa7b4d4e444a40ec277045f4c7378d
Bradley Beggs <brad.beggs@gmail.com>
2020-06-08 14:28:26 +0000

    Automatically backed up by Learn

--- flow-control.js
+++ flow-control.js
@@ -16,7 +16,7 @@ function teenager(age) {
       return "You are not a teenager"
   }
 }
-/*
+
 function ageChecker(age) {
   if (age >= 13 && age <= 19)
     {      return "You are a teenager!"  }
@@ -28,7 +28,7 @@ function ageChecker(age) {
   {You are a growup}
 
 }
-
+/*
 function ternaryTeenager(age) {
 
 }
